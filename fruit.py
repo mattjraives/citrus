97b51f18bb236683aa0432b171d81413a2e5f538
Matthias J. Raives <mattjraives@users.noreply.github.com>
2018-02-22 10:18:43 -0500

    Create fruit.py

---
+++ fruit.py
@@ -0,0 +1,17 @@
+class Fruit:
+  def __init__(self,name,parents):
+    self.name = name
+    self.parents = parents
+    self.children = []
+    self.family = []
+    self.siblings = []
+  def find_children(self,basket): # basket is a list of Fruit objects
+    for fruit in basket:
+      if fruit.name is not self.name:
+        if self.name in [parent.name for parent in fruit.parents]:
+          self.children.append(fruit)
+    self.family = self.parents + self.children
+  def find_siblings(self,basket):
+    for fruit in basket:
+      if fruit.name is not self.name:
+        if fruit.
